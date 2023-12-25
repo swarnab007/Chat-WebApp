7238032ede17ea4e7b4544e3f7f488ff42e23baf
Swarnab Banerjee <116362013+swarnab007@users.noreply.github.com>
2023-12-25 08:50:16 +0530

    Update Register.jsx

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import add from "../images/addAvatar.png";
-import { sendSignInLinkToEmail, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, storage, db } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
@@ -8,52 +8,46 @@ import { doc, setDoc } from "firebase/firestore";
 const Register = () => {
   const [err, setErr] = useState(false);
   const handleSubmit = async (e) => {
-
     // it will stop the page from reloading
     e.preventDefault();
-
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
-    const file = e.target[3].value;
+    const file = e.target[3].files[0];
 
-    // create user
-    const res = await sendSignInLinkToEmail(auth, email, actionCodeSettings)
-      .then(() => {
-        const storageRef = ref(storage, displayName);
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
-        const uploadTask = uploadBytesResumable(storageRef, file);
+      const storageRef = ref(storage, displayName);
 
-        // Register three observers:
-        uploadTask.on(
-          (err) => {
-            // Handle unsuccessful uploads
-            setErr(true);
-          },
-          () => {
-            // Download url
-            getDownloadURL(uploadTask.snapshot.ref).then(
-              async (downloadURL) => {
-                await updateProfile(res.user, {
-                  displayName,
-                  photoURL: downloadURL,
-                });
-                console.log(downloadURL);
-                // create user on firestore
-                await setDoc(doc(db, "users", res.user.uid), {
-                  uid: res.user.uid,
-                  displayName,
-                  email,
-                  photoURL: downloadURL,
-                });
-              }
-            );
-          }
-        );
-      })
-      .catch((err) => {
-        setErr(true);
-      });
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observersers
+      uploadTask.on(
+        (err) => {
+          setErr(true);
+        },
+        () => {
+          // Handle successful uploads on complete
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            // update profile
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            // storing data in firebase
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } catch (err) {
+      setErr(true);
+    }
   };
 
   return (
@@ -61,7 +55,7 @@ const Register = () => {
       <div className="formWrapper">
         <span className="logo">SWARNAB Chat</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="Username" />
           <input required type="email" placeholder="email" />
           <input required type="password" placeholder="password" />
@@ -70,8 +64,7 @@ const Register = () => {
             <img src={add} alt="" />
             <span>Add an avatar</span>
           </label>
-          <button onSubmit={handleSubmit}>Sign up</button>
-          {err && <span>Something went wrong</span>}
+          <button>Sign up</button>
         </form>
         <p>You do have an account? Login</p>
       </div>
