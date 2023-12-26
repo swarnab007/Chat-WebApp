09e72978bfb84fdcd3b9f8d0c8bc2143e35cbb03
Swarnab Banerjee <116362013+swarnab007@users.noreply.github.com>
2023-12-26 07:53:41 +0530

    pushed

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -4,9 +4,12 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, storage, db } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+  
   const handleSubmit = async (e) => {
     // it will stop the page from reloading
     e.preventDefault();
@@ -42,6 +45,9 @@ const Register = () => {
               email,
               photoURL: downloadURL,
             });
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
           });
         }
       );
